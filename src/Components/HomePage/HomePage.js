034c61657e52709abc27288c4ff78c83093d09ce
aaditya-20 <91070772+aaditya-20@users.noreply.github.com>
2022-11-15 14:34:09 +0530

    Update HomePage.js
    comment added

--- src/Components/HomePage/HomePage.js
+++ src/Components/HomePage/HomePage.js
@@ -4,8 +4,14 @@ import Banner from "./Sections/Banner";
 import KnowAbout from "./Sections/KnowAbout";
 import JobSeeker from "./Sections/JobSeeker";
 import Employer from "./Sections/Employer";
-
+// it means a class named homepage is created whose parent class is component
 class HomePage extends Component {
+  // render is method in Homepage class component
+  // A class component must include the extends React.Component statement.
+  //  This statement creates an inheritance to React.Component, 
+  // and gives your component access to React.Component's functions.
+
+// render means to present something after execution()
   render() {
     return (
       <div>
