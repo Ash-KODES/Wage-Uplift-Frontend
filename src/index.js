8158e6d7b74a3b29aee32ca979ba398514ebef1e
Ash-KODES <choudharykumarashutosh1209@gmail.com>
2023-01-08 12:05:55 +0530

    Comment added and refactoring

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "./i18n";
-
+// Entry point Application
 ReactDOM.render(
   <React.StrictMode>
     <App />
--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -1,3 +1,5 @@
+//Service Worker script
+
 // This optional code is used to register a service worker.
 // register() is not called by default.
 
